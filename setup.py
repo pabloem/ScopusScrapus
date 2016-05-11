fa77428dc89ea4572ec25623caa85ee83a8d74ac
pabloem <polecito.em@gmail.com>
2016-05-11 21:37:24 +0900

    FixedRelease 0.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'ScopusScrapus',
   packages = ['ScopusScrapus'],
-  version = '0.1',
+  version = '0.0.2',
   description = 'Small lib to get data from Elsevier\'s Scopus API',
   author = 'Pablo E.',
   author_email = 'mail@iampablo.me',
   url = 'https://github.com/pabloem/ScopusScrapus',
-  download_url = 'https://github.com/pabloem/ScopusScrapus/tarball/0.0.1',
+  download_url = 'https://github.com/pabloem/ScopusScrapus/tarball/0.0.2',
   keywords = ['scopus', 'scrape', 'elsevier'],
   classifiers = [],
 )
